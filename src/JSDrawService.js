8aad9b8719c54ede87b0ba7137839d694a0164c9
Gerry Zhou <guangxu.zhou@fda.hhs.gov>
2020-02-28 16:27:42 -0600

    delete service.aspx

--- src/JSDrawService.js
+++
@@ -1,3 +0,0 @@
-JSDraw2.licensecode='405432435826116689283014124368927817617232424242424241516171616171616171616231217161617161717171723141512181';
-JSDrawServices={url:'http://www.elncloud.com/jsdrawapp/jsdraw/Service.aspx',n2s:{url:'http://www.elncloud.com/jsdrawapp/jsdraw/Service.aspx?cmd=n2s.lookup',suggest:'http://www.elncloud.com/jsdrawapp/jsdraw/Service.aspx?cmd=n2s.suggest'},id2s:{regex:null,url:'http://www.elncloud.com/jsdrawapp/jsdraw/Service.aspx?cmd=n2s.lookup'},scifinder:{clientid:'brsh9f5gmu'}};
-if(scil.SciFinder!=null)scil.SciFinder.clientid='brsh9f5gmu';
